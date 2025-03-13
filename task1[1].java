06263a3c2d179fc28c37fc08239b42e415b727f6
Uday1769 <n.uday1769@gmail.com>
2025-03-13 12:21:25 +0530

    Add files via upload

---
+++ task1[1].java
@@ -0,0 +1,13 @@
+package day1;
+import java.util.Scanner;
+
+public class task1 {
+	public static void main(String args[])
+	{
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String message = scanner.nextLine();
+        System.out.println("Have a nice day "+message);
+	}
+
+}
